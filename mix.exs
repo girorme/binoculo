8494d106a3054aa48ca4944a8c20f6dad55c1ba4
girorme <g1r0rm3@gmail.com>
2023-05-05 17:56:51 -0300

    ignore modules on cover

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule BinoculoDaemon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [ignore_modules: [BinoculoDaemon]]
     ]
   end
 
