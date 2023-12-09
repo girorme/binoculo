ac25b6f8d046b81584f8de5281b663a644ef2cf5
girorme <g1r0rm3@gmail.com>
2023-12-09 18:48:02 -0300

    improve dashboard style

--- dashboard/src/app.js
+++ dashboard/src/app.js
@@ -33,18 +33,24 @@ search.addWidgets([
     transformItems(items) {
       return items.map(item => {
         item.response = item.response.split("\r\n").map((item) => `<p>${item}</p>`).join("")
+
+        if(item.response.length > 10) item.response = item.response.substring(0,250) + "..."
         return item
       });
     },
     templates: {
       item(hit, { html, components, sendEvent }) {
         return `
-          <p>Host: ${hit.host}</p>
-          <p>Port: ${hit.port}</p>
-          <br>
-          <p>Response</p>
-          <hr>
-          <p>${hit.response}</p>
+          <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <a href="#">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    ${hit.host}:${hit.port}
+                  </h5>
+              </a>
+              <p class="mb-3 text-slate-400">
+                ${hit.response}
+              </p>
+          </div>
         `;
       },
     }
