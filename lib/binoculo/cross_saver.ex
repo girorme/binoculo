d3b7d06c08c6bfc62413f160fa12369c5cf17fae
girorme <g1r0rm3@gmail.com>
2023-12-30 11:56:22 -0300

    improve code order

--- lib/binoculo/cross_saver.ex
+++ lib/binoculo/cross_saver.ex
@@ -17,10 +17,7 @@ defmodule Binoculo.CrossSaver do
   defp save_to_file_enabled?(), do: Application.get_env(:binoculo, :save_to_file, false)
 
   defp save_to_file(false), do: :noop
-  defp save_to_file(response) when is_binary(response) do
-    String.to_atom(response)
-    |> save_to_file()
-  end
+
   defp save_to_file(true) do
     results =
       Results.get_finished()
@@ -28,5 +25,10 @@ defmodule Binoculo.CrossSaver do
 
     Config.get_output_file()
     |> File.write(results, [:append])
+  end
+
+  defp save_to_file(response) when is_binary(response) do
+    String.to_atom(response)
+    |> save_to_file()
   end
 end
