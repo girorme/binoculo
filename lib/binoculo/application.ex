47021ae6f4e61e8706ee5e34709d4a7474ae1f68
girorme <g1r0rm3@gmail.com>
2024-01-05 17:42:48 -0300

    improve maestro init

--- lib/binoculo/application.ex
+++ lib/binoculo/application.ex
@@ -4,20 +4,13 @@ defmodule Binoculo.Application do
   """
   use Application
 
-  alias Binoculo.{Config, Maestro}
+  alias Binoculo.Config
 
   @impl true
   def start(_type, _args) do
     children = [
       {Config, %{}}
     ]
-
-    children =
-      if System.get_env("PROD") do
-        children ++ [Maestro]
-      else
-        children
-      end
 
     opts = [strategy: :one_for_one, name: Binoculo.Supervisor]
     Supervisor.start_link(children, opts)
