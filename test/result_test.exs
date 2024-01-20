d81f55c24249195134efccbc49b17017fb169221
girorme <g1r0rm3@gmail.com>
2024-01-20 03:39:50 -0300

    improve finish test

--- test/result_test.exs
+++ test/result_test.exs
@@ -13,7 +13,6 @@ defmodule ResultTest do
       host_info_ut = %{host: "127.0.0.1", port: 21_210}
       assert Results.add_item(host_info_ut) == :ok
       assert Results.get_qty_running() == 1
-      assert Results.get_finished() == []
 
       running = Results.get_running() |> Enum.map(fn %{host: host} -> host end)
       assert host_info_ut.host in running
@@ -32,17 +31,17 @@ defmodule ResultTest do
       assert host_info_ut.host in finished
     end
 
-    test "should only remove item from progress" do
+    test "should remove specified finished item from progress" do
       host_info_ut = %{host: "127.0.0.1", port: 21_210}
       Results.add_item(host_info_ut)
-      Results.remove_item(host_info_ut)
+      Results.finish_item(host_info_ut)
 
       finished =
         Results.get_finished()
         |> Enum.map(& &1[:host])
 
       assert Results.get_qty_running() == 0
-      assert host_info_ut.host not in finished
+      assert host_info_ut.host in finished
     end
   end
 
