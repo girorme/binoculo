67ac62499690d9cb947be743c0df1a4e0c2fe9db
girorme <rodrigogirorme@gmail.com>
2025-01-24 01:06:08 -0300

    chore: improve args logic and coverage

--- lib/binoculo/args.ex
+++ lib/binoculo/args.ex
@@ -8,87 +8,90 @@ defmodule Binoculo.Args do
   def parse_args(argv) do
     version = Util.version()
 
-    Optimus.new!(
-      name: "Binoculo",
-      description: "Binoculo: You Know, for Banner Grabbing!",
-      version: "Version: #{version}",
-      author: "Author: Girorme <g1r0rm3@gmail.com>",
-      about: "A banner grabbing tool",
-      allow_unknown_args: false,
-      parse_double_dash: true,
-      flags: [
-        verbosity: [
-          short: "-v",
-          help: "Verbosity level",
-          multiple: true
-        ]
-      ],
-      options: [
-        host_notation: [
-          value_name: "host_notation",
-          long: "--range",
-          help: "CIDR or IP range: 192.168.1.0/24 or 192.168.1.0..192.168.1.255",
-          parser: fn notation ->
-            case Util.parse_range_or_cidr_notation(notation) do
-              {:error, _} -> {:error, "invalid cidr or notation"}
-              {:ok, _} -> {:ok, notation}
-            end
-          end,
-          required: true
+    optimus =
+      Optimus.new!(
+        name: "Binoculo",
+        description: "Binoculo: You Know, for Banner Grabbing!",
+        version: "Version: #{version}",
+        author: "Author: Girorme <g1r0rm3@gmail.com>",
+        about: "A banner grabbing tool",
+        allow_unknown_args: false,
+        parse_double_dash: true,
+        flags: [
+          verbosity: [
+            short: "-v",
+            help: "Verbosity level",
+            multiple: true
+          ]
         ],
-        ports: [
-          value_name: "port(s)",
-          short: "-p",
-          long: "--port",
-          help: "Port(s) to scan: 80,443,8080 or 80-8080 or 21,80-8080",
-          parser: fn port ->
-            case parse_port_arg(port) do
-              :error -> {:error, "invalid port(s)"}
-              {:ok, port_parsed} -> {:ok, port_parsed}
-            end
-          end,
-          required: true
-        ],
-        output: [
-          value_name: "output",
-          short: "-o",
-          long: "--output",
-          help: "Output file",
-          parser: fn output ->
-            case File.open("output/#{output}", [:append]) do
-              {:ok, _file} ->
-                {:ok, output}
+        options: [
+          host_notation: [
+            value_name: "host_notation",
+            long: "--range",
+            help: "CIDR or IP range: 192.168.1.0/24 or 192.168.1.0..192.168.1.255",
+            parser: fn notation ->
+              case Util.parse_range_or_cidr_notation(notation) do
+                {:error, _} -> {:error, "invalid cidr or notation"}
+                {:ok, _} -> {:ok, notation}
+              end
+            end,
+            required: true
+          ],
+          ports: [
+            value_name: "port(s)",
+            short: "-p",
+            long: "--port",
+            help: "Port(s) to scan: 80,443,8080 or 80-8080 or 21,80-8080",
+            parser: fn port ->
+              case parse_port_arg(port) do
+                {:error, _} -> {:error, "invalid port(s)"}
+                {:ok, port_parsed} -> {:ok, port_parsed}
+              end
+            end,
+            required: true
+          ],
+          output: [
+            value_name: "output",
+            short: "-o",
+            long: "--output",
+            help: "Output file",
+            parser: fn output ->
+              # check if output dir exists and create it if not
+              File.mkdir_p!("output")
 
-              {:error, _} ->
-                if Mix.env() == :test do
-                  {:ok, output}
-                else
-                  {:error, "invalid output file"}
-                end
+              with {:ok, _file} <- File.open("output/#{output}", [:append]) do
+                {:ok, output}
+              end
             end
-          end
-        ],
-        write: [
-          value_name: "write",
-          short: "-w",
-          long: "--write",
-          help: "Write cutom payload to socket, e.g: GET / HTTP/1.1"
-        ],
-        read: [
-          value_name: "read",
-          short: "-r",
-          long: "--read",
-          help: "Save only responses that match with this string, e.g: Apache | nginx,php",
-          parser: fn read_payload ->
-            case String.contains?(read_payload, ",") do
-              true -> {:ok, String.split(read_payload, ",")}
-              false -> {:ok, read_payload}
+          ],
+          write: [
+            value_name: "write",
+            short: "-w",
+            long: "--write",
+            help: "Write cutom payload to socket, e.g: GET / HTTP/1.1"
+          ],
+          read: [
+            value_name: "read",
+            short: "-r",
+            long: "--read",
+            help: "Save only responses that match with this string, e.g: Apache | nginx,php",
+            parser: fn read_payload ->
+              case String.contains?(read_payload, ",") do
+                true -> {:ok, String.split(read_payload, ",")}
+                false -> {:ok, read_payload}
+              end
             end
-          end
+          ]
         ]
-      ]
-    )
-    |> Optimus.parse!(argv)
+      )
+
+    case String.to_atom(System.get_env("MIX_ENV", "prod")) do
+      :test ->
+        Optimus.parse(optimus, argv)
+
+      _ ->
+        {:ok, Optimus.parse!(optimus, argv)}
+    end
   end
 
   defp parse_port_arg(port_arg) do
@@ -106,35 +109,37 @@ defmodule Binoculo.Args do
     {invalid_ports, ports} = ports
 
     if Enum.count(invalid_ports) > 0 do
-      :error
+      {:error, "invalid port(s): #{inspect(invalid_ports)}"}
     else
-      {:ok, parse_valid_ports(ports)}
+      Enum.map(ports, fn {:ok, port} -> port end)
+      |> generate_port_range()
     end
   end
 
-  defp parse_valid_ports(ports) do
-    Enum.map(ports, fn {_, port} ->
-      case String.split(port, "-") do
-        [port] ->
-          String.to_integer(port)
-
-        [port_start, port_end] ->
-          range_from_port(port_start, port_end)
+  def generate_port_range(ports) do
+    ports
+    |> Enum.reduce_while({:ok, []}, fn port, {:ok, acc} ->
+      case parse_port(port) do
+        {:ok, port_list} -> {:cont, {:ok, acc ++ port_list}}
+        {:error, reason} -> {:halt, {:error, reason}}
       end
     end)
-    |> List.flatten()
-    |> Enum.sort()
   end
 
-  defp range_from_port(port_start, port_end) do
-    port_start = String.to_integer(port_start)
-    port_end = String.to_integer(port_end)
+  defp parse_port(port) do
+    case String.split(port, "-") do
+      [single_port] ->
+        {:ok, [String.to_integer(single_port)]}
 
-    if port_start > port_end do
-      IO.puts("Invalid port range: #{port_start} > #{port_end}")
-      System.halt(0)
+      [start_port, end_port] ->
+        start_port = String.to_integer(start_port)
+        end_port = String.to_integer(end_port)
+
+        if start_port <= end_port do
+          {:ok, Enum.to_list(start_port..end_port)}
+        else
+          {:error, "Invalid range: start port must be less than or equal to end port"}
+        end
     end
-
-    Enum.to_list(port_start..port_end)
   end
 end
