efd517622d94a591d76ab6d2fb179689052e4f0c
girorme <rodrigogirorme@gmail.com>
2025-01-24 01:12:47 -0300

    chore: update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Binoculo.MixProject do
   def project do
     [
       app: :binoculo,
-      version: "1.1.1",
+      version: "1.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
